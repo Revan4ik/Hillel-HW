f306a64334ccc224bd1e558fd769f49edb2b523b
Revan4ik <gaetndyuude@gmail.com>
2022-04-14 00:23:21 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,76 @@
+var min = +prompt ("Введите число от 0 до 59");
+if (min >= 0 && min <= 14) {
+    console.log('ваше число в 1 четверти');
+} else if (min >= 15 && min <= 29 ) {
+    console.log('Ваше число в 2 четверти');
+} else if (min >= 30 && min <= 44 ){
+    console.log('Ваше число в 3 четверти');
+} else if (min>= 45 && min <= 59 ){ 
+    console.log('Ваше число в 4 четвери');
+} else {console.log('Вы ввели не правильное число')}
 
-var name = prompt ("What is your name?")
-alert("Hello, " +name +'! How are you?')
+
+
+ var a = '1';
+ if (a === '1'){ 
+    console.log('Верно');
+ } else console.log('Неверно');
+ var a = 1;
+ if (a === '1'){ 
+    console.log('Верно');
+ } else console.log('Неверно');
+ var a = 3;
+ if (a === '1'){ 
+    console.log('Верно');
+ } else console.log('Неверно');
+
+
+
+ var test = confirm('yes/no');
+ if (test == false){
+    console.log('Верно');
+ } else {
+    console.log('Неверно')
+ }
+test == false ? console.log('Верно') : console.log('Неверно');
+
+
+
+var a = 5
+if (a > 0 && a < 5){
+    console.log('Верно')
+} else {
+    console.log('Неверно')
+}
+var a = 0
+if (a > 0 && a < 5){
+    console.log('Верно')
+} else {
+    console.log('Неверно')
+}var a = -3
+if (a > 0 && a < 5){
+    console.log('Верно')
+} else {
+    console.log('Неверно')
+}var a = 2
+if (a > 0 && a < 5){
+    console.log('Верно')
+} else {
+    console.log('Неверно')
+}
+
+
+
+var num = +prompt('Введите число от 1 до 4')
+if (num == 1){
+    var result = 'зима'
+} else if (num == 2){
+    var result = 'весна'
+} else if (num == 3){
+    var result = 'лето'
+} else if (num == 4){
+    var result = 'осень'
+} else {
+    var result = 'ввели неправильное число'
+}
+console.log(result)
