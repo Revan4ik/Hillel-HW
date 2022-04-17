7d2b8508643a235df03a40445975ed0b181caaad
Revan4ik <gaetndyuude@gmail.com>
2022-04-17 17:02:08 +0300

    HW4

--- js.js
+++ js.js
@@ -1,3 +1,71 @@
+// Task
+var sum = '';
+var str = '';
+for (var i = 0; i < 7; i++){
+    str = str + '#';
+    sum = sum + str + '\n'
+}
+console.log(sum)
 
-var name = prompt ("What is your name?")
-alert("Hello, " +name +'! How are you?')
+// bonus Task
+var sum = ''
+for (var i = 0; i < 27; i++){
+
+    if(i == 0) sum = sum + '############################'  + '\n'
+    if(i == 1){
+        for (var j = 0; j < 2; j++){
+            sum = sum + '####' + "                    "
+        }
+        sum = sum + '\n'
+    } 
+        if(i == 2) {
+        for (var j = 0; j < 1; j++){
+            sum = sum + '##' + "   " + '***' + "                " + '**' + '##'
+        }
+        sum = sum + '\n'
+    } 
+        if(i == 3){
+        for (var j = 0; j < 1; j++){
+            sum = sum + '#' + "   " + "*##**" + "         " + "**" + "  " + "0" + "  " + "*##"
+        }
+        sum = sum + '\n'
+    }
+    if(i == 4){
+        for (var j = 0; j < 1; j++){
+            sum = sum + '#' + "    " + "***" + "     " + "0" + "    " + "##**" + "    " + "*#"
+        }
+        sum = sum + '\n'
+    }
+    if(i == 5){
+        for (var j = 0; j < 1; j++){
+            sum = sum + '#' + "       " + "0" + "         " + "##***" + "    " + "#"
+        }
+        sum = sum + '\n'
+    }
+    if(i == 6){
+        for (var j = 0; j < 1; j++){
+            sum = sum + '#' + "                 " + "##**" + "     " + "#"
+        }
+        sum = sum + '\n'
+    }
+    if(i == 7){
+        for (var j = 0; j < 1; j++){
+            sum = sum + '#' + "   " + "0" + "       " + "#*" + "             " + "#"
+        }
+        sum = sum + '\n'
+    }
+    if(i == 8){
+        for (var j = 0; j < 1; j++){
+            sum = sum + '#***' + "        " + "##**" + "    " + "0" + "    " + "**#"
+        }
+        sum = sum + '\n'
+    }
+    if(i == 9){
+        for (var j = 0; j < 1; j++){
+            sum = sum + '##****' + "     " + "###***" + "       " + "*###"
+        }
+        sum = sum + '\n'
+    }
+    if(i == 10) sum = sum + '############################'  + '\n'
+}
+console.log(sum)
