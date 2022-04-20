ea4116570ecc1925337eb2c413b6b7a189ad41cd
Revan4ik <gaetndyuude@gmail.com>
2022-04-20 03:04:10 +0300

    4hw

--- js.js
+++ js.js
@@ -1,3 +1,38 @@
+// HW3
 
-var name = prompt ("What is your name?")
-alert("Hello, " +name +'! How are you?')
+for (;;) {
+  op = prompt("Выбери действие (+,-,*,/)");
+  if (
+    op === "+" || op === "-" || op === "/" || op === "*"
+  )
+    break;
+}
+
+for (;;) {
+  countoper = +prompt("Введите количество операндов");
+  if (countoper > 1) break;
+}
+
+for (var i = 0; i < countoper; i++) {
+  var oper = +prompt("Введите ваши операнды");
+  if (i === 0) {
+    result = oper;
+    continue;
+  }
+  switch (op) {
+    case "+":
+      result += oper;
+      break;
+    case "-":
+      result -= oper;
+      break;
+    case "*":
+      result *= oper;
+      break;
+    case "/":   
+      result /= oper;
+      break;
+  }
+}
+
+alert("Ваш результат " + result);
