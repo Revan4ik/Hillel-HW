d90af2cf1046330916efe10d6b40282a7672ce59
Revan4ik <gaetndyuude@gmail.com>
2022-05-11 18:54:55 +0300

    Update js.js

--- js.js
+++ js.js
@@ -11,3 +11,21 @@ console.log('not valid password =>', test('qqqqqq'));
 
 //Task 2
 
+function getRandom() {
+    var arr = [];
+    return function getNumbers() {
+      var maxNumber = 100;
+      var random = Math.ceil(Math.random() * maxNumber);
+      if (arr.includes(random)) {
+        return getNumbers();
+      } else {
+        arr.push(random);
+        return random;
+      }
+    };
+  }
+  
+  var test2 = getRandom();
+  for (var i = 0; i < 100; i++) {
+    console.log('Number =>', test2());
+  }
