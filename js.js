f5b718f1dc32d26bf7fac79a2d01c0b68323a64f
Revan4ik <gaetndyuude@gmail.com>
2022-05-15 19:27:56 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,14 @@
-
-var name = prompt ("What is your name?")
-alert("Hello, " +name +'! How are you?')
+// alert
+var timetowakeup = prompt("wake up in (hh:mm)")
+var timetowakeuparr = timetowakeup.split(':').join('')
+var time3 = setInterval(clock, 1000);
+function clock(){
+    var date = new Date();
+    var time = date.toLocaleTimeString()
+    var time2 = time.split(':').slice(0,-1).join('')
+    document.getElementById('block').innerHTML = time
+    if(time2 == timetowakeuparr) {
+    alert('time to wake up')
+    clearInterval(time3);
+}
+}
