75a74433a30f797215a56f7af25dc37bc86988ee
Revan4ik <gaetndyuude@gmail.com>
2022-06-15 00:59:21 +0300

    Create Car.js

---
+++ js/Car.js
@@ -0,0 +1,11 @@
+class Car {
+    constructor(name, price){
+        this.name = name 
+        this.price = price
+    }
+    Checkcar(){
+        return console.log('')
+    }
+}
+const cars = new Car ('Renault', '1500$')
+console.log(cars)
