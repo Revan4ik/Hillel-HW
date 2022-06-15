b6593fb5838677a76010dd11922613918ed7d621
Revan4ik <gaetndyuude@gmail.com>
2022-06-15 22:15:11 +0300

    Update Childs.js

--- js/Childs.js
+++ js/Childs.js
@@ -14,9 +14,9 @@ const Piston = new Child1and2('Piston', '99$', '99%', '2hours')
 const cylinder = new Child1and2('cylinder', '400$', '90%', '1day')
 console.log(Piston)
 console.log(cylinder)
-class Child3and4 extends Descendants1{
-    constructor(name, price, reliability, areavailable){
-        super(name, price,reliability)
+class Child3and4 extends Descendants2{
+    constructor(name, price, type, areavailable){
+        super(name, price,type)
         this.areavailable = areavailable
     }
     checkClutch(){
@@ -26,13 +26,13 @@ class Child3and4 extends Descendants1{
         return console.log('')
     }
 }
-const Clutch = new Child3and4('Clutch', '92$', '46%', 'true')
-const differential = new Child3and4('differential', '200$', '99%', 'false')
+const Clutch = new Child3and4('Clutch', '92$', 'back', 'true')
+const differential = new Child3and4('differential', '200$', 'front', 'false')
 console.log(Clutch)
 console.log(differential)
-class Child5and6 extends Descendants1{
-    constructor(name, price, reliability, weight){
-        super(name, price,reliability)
+class Child5and6 extends Descendants3{
+    constructor(name, price, amount, weight){
+        super(name, price, amount)
         this.weight = weight
     }
     checkshockabsorberpiston(){
@@ -42,7 +42,7 @@ class Child5and6 extends Descendants1{
         return console.log('')
     }
 }
-const shockabsorberpiston = new Child5and6('shockabsorberpiston', '15$', '99%', '100g')
-const storagetank = new Child5and6('storagetank', '30$', '90%', '200g')
+const shockabsorberpiston = new Child5and6('shockabsorberpiston', '15$', '2', '100g')
+const storagetank = new Child5and6('storagetank', '30$', '3', '200g')
 console.log(shockabsorberpiston)
 console.log(storagetank)
